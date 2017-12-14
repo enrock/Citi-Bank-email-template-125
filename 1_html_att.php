850e95653583e522f8cbe53f37c5fb6a374429a6
Enrique Quiroga <enrique.soler@heathwallace.com>
2017-12-14 12:54:50 -0600

    First commit

---
+++ 1_html_att.php
@@ -0,0 +1,9 @@
+
+<?PHP 
+	/* THIS IS WHERE THE CITI HEADER HTML IS INSERTED */
+	include "../footer-header/122_att_header.html" ;
+	/* THIS IS WHERE THE CITI BODY HTML IS INSERTED */
+	include "ERN002_01.html" ;
+	/* THIS IS WHERE THE CITI FOOTER HTML IS INSERTED */
+	include "../footer-header/att_footer_black_v10.html" ; 
+?>
---
+++ 1_html_citi.php
@@ -0,0 +1,9 @@
+
+<?PHP 
+	/* THIS IS WHERE THE CITI HEADER HTML IS INSERTED */
+	include "../footer-header/122_citi_header.html" ;
+	/* THIS IS WHERE THE CITI BODY HTML IS INSERTED */
+	include "ERN002_01.html" ;
+	/* THIS IS WHERE THE CITI FOOTER HTML IS INSERTED */
+	include "../footer-header/citi_footer_black_v14.html" ; 
+?>
